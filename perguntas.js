e814325810f84683ce108f85ded935fb99598c94
isinha018 <162440838+isinha018@users.noreply.github.com>
2024-11-06 08:03:43 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Desenho',
+    'Quem é o vilão que está sempre tentando capturar o "pernalonga" nos dos desenhos da warner bros?',
+    'Hortelino troca letras' 
+)
+
+criaCartao(
+    'Artes.'
+    'Que famosa obra e arte de Leonardo da Vinci retrato uma mulher com um sorriso enigmático?',
+    'Monalisa'
+)
+
+criaCartao(
+    'Música',
+    'O que é hormonia em música?',
+    'harmonia é a combinação e notas tocadas simultamente para criar acordes e suportar a melodia'
+)
+
+criaCartao(
+    'Música',
+    'O que é uma chave na notação musical?',
+    'a chave é um símbolo colocado no início de uma pauta musical que indica a altura das notas em relação. '
+)
